4b09fd067bb09d47e61fbbad127c4c48d550e848
prithviraj <92168367+prithvi206@users.noreply.github.com>
2023-06-23 17:11:00 +0530

    Update page.tsx
    added Home props

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import { Hero,CustomFilter,SearchBar, CarCard} from './components'
 import { fetchCars } from '@/utils'
 import { fuels, yearsOfProduction } from './constants';
-import { HomeProps } from "../types";
+import { HomeProps } from "./types";
 
 
 export default async function Home({ searchParams }: HomeProps) {
