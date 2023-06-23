3a3a23f3d138fd79ad7ebaa427e9783bd8057174
prithviraj <92168367+prithvi206@users.noreply.github.com>
2023-06-23 17:03:43 +0530

    Update page.tsx
    added Home Props

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { fetchCars } from '@/utils'
 import { fuels, yearsOfProduction } from './constants';
 
 
-export default async function Home({searchParams}) {
+export default async function Home({ searchParams }: HomeProps}) {
   const allCars = await fetchCars({
     manufacturer: searchParams.manufacturer || '', year: searchParams.year || 2022, fuel: searchParams.fuel || '',
     limit: searchParams.limit || 10,
